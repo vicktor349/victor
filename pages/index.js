53df6fcaeec9bc0f6dc48294618a2d05349b299b
vicktor349 <ayomidevictor349@gmail.com>
2024-08-29 22:29:01 +0100

    calculated height minusing navbar component

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Home()
 {
   return (
-    <main className="text-white">
+    <main>
+      <div className="flex items-center justify-center h-[calc(100vh-56.19px)]">
+        <p>hello world</p>
+      </div>
     </main>
   )
 }
