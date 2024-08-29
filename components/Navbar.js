0db5c4e93fa78fd5939e24e5e7b024a9b764a583
vicktor349 <ayomidevictor349@gmail.com>
2024-08-30 00:06:26 +0100

    Removed unneccessary footer

--- components/Navbar.js
+++ components/Navbar.js
@@ -68,7 +68,7 @@ const Navbar = () =>
                 {/* MOBILE DRAWER */}
                 {
                     isOpened && (
-                        <div className="absolute top-14 w-full bg-bgColor h-dvh">
+                        <div className="absolute top-14 w-full bg-bgColor h-screen z-[999999]">
                             {navlinks.map((navlink) => (
                                 <div key={navlink.id}>
                                     <div className="border-borderColor border-b py-4">
@@ -76,20 +76,6 @@ const Navbar = () =>
                                     </div>
                                 </div>
                             ))}
-                            <div className="fixed bottom-0 left-0 w-full flex items-center justify-between px-5 h-[49px] border-t border-borderColor">
-                                <p>find me in:</p>
-                                <div className="flex items-center gap-x-6">
-                                    <span className="relative flex items-center">
-                                        <BsTwitterX />
-                                        <span className="absolute right-[-12px] w-[1px] h-6 bg-borderColor"></span>
-                                    </span>
-                                    <span className="relative flex items-center">
-                                        <BsWhatsapp />
-                                        <span className="absolute right-[-12px] w-[1px] h-6 bg-borderColor"></span>
-                                    </span>
-                                    <BsTelegram />
-                                </div>
-                            </div>
                         </div>
                     )
                 }
