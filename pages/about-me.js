35e4a4cfac3dbdf709d68203c9f776abe008ff72
vicktor349 <ayomidevictor349@gmail.com>
2024-08-30 13:53:36 +0100

    About me page

---
+++ pages/about-me.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const aboutme = () =>
+{
+    return (
+        <div className="flex items-center justify-center h-[calc(100vh-56.19px)]">
+            about me
+        </div>
+    )
+}
+
+export default aboutme
