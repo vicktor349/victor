119227d9a63ccfdd4e7dfdd5b45318bc704fe8bb
vicktor349 <ayomidevictor349@gmail.com>
2024-08-30 13:49:20 +0100

    Footer staying at bottom of page

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import { BsGithub, BsTwitterX, BsWhatsapp } from 'react-icons/bs'
 const Footer = () =>
 {
     return (
-        <div className="text-primary border-borderColor border-t w-full fixed">
+        <div className="text-primary border-borderColor border-t w-full mt-auto">
             <div className="flex ssm:justify-around ssssm:justify-between">
                 <div className="flex items-center h-14">
                     <p className="ssm:w-32 ssssm:w-44 2xl:w-48 3xl:w-56 border-r border-borderColor h-full flex items-center justify-center">Find me in:</p>
