a421628aeac82eea9d84a853dfa9f252ab7fccb0
vicktor349 <ayomidevictor349@gmail.com>
2024-09-02 12:15:04 +0100

    About me page

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,12 +1,53 @@
-import React from 'react'
+import AboutContent from '@/components/aboutme/AboutContent';
+import AboutDisplay from '@/components/aboutme/AboutDisplay';
+import Hobbies from '@/components/aboutme/Hobbies/Hobbies';
+import Personal from '@/components/aboutme/personal/Personal';
+import Professional from '@/components/aboutme/Professional/Professional';
+import SideNavbar from '@/components/aboutme/SideNavbar';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react';
+
 
 const aboutme = () =>
 {
-    return (
-        <div className="flex items-center justify-center h-[calc(100vh-56.19px)]">
-            about me
-        </div>
-    )
-}
+    const router = useRouter()
+    const [activeComponent, setActiveComponent] = useState("Personal");
 
-export default aboutme
+
+    const handleSetActiveComponent = (component) =>
+    {
+        setActiveComponent(component);
+    };
+
+    const renderComponent = () =>
+    {
+        switch (activeComponent)
+        {
+            case "Personal":
+                return <Personal setActiveComponent={handleSetActiveComponent} />
+            case "Professional":
+                return <Professional setActiveComponent={handleSetActiveComponent} />
+            case "Hobbies":
+                return <Hobbies setActiveComponent={handleSetActiveComponent} />
+        }
+    };
+
+    return (
+        <div className="lg:flex lg:h-[calc(100vh-56.19px)] text-primary">
+            <div className='pt-[59.19px] border-borderColor border-b w-full flex items-center h-32 text-white pl-8 lg:hidden text-sm ssssm:text-base'>
+                <p>_about-me</p>
+            </div>
+            <div className="lg:h-full">
+                <SideNavbar activeComponent={activeComponent} setActiveComponent={handleSetActiveComponent} />
+            </div>
+            <div className="lg:flex-2 lg:flex items-center lg:justify-center space-y-1">
+                <AboutContent renderComponent={renderComponent} />
+            </div>
+            <div className='flex-1'>
+                <AboutDisplay />
+            </div>
+        </div>
+    );
+};
+
+export default aboutme;
