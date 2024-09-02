e7f6b968250f8afd9922cd0a9b9054f5507ba6b7
vicktor349 <ayomidevictor349@gmail.com>
2024-09-02 17:39:16 +0100

    Displaying content

--- components/aboutme/AboutDisplay.js
+++ components/aboutme/AboutDisplay.js
@@ -1,19 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { IoMdClose } from 'react-icons/io';
+import { useFile } from '../FileContext';
 
 const AboutDisplay = () =>
 {
+    const { openedFiles, activeFile, openFile, closeFile } = useFile();
+    const [components, setComponents] = useState({});
+
+    useEffect(() =>
+    {
+        openedFiles.forEach((file) =>
+        {
+            if (!components[file])
+            {
+                import(`../../data/${file}`)
+                    .then((module) =>
+                    {
+                        setComponents((prev) => ({
+                            ...prev,
+                            [file]: module.default,
+                        }));
+                    })
+                    .catch((err) =>
+                    {
+                        console.error('Failed to load the component:', err);
+                        setComponents((prev) => ({
+                            ...prev,
+                            [file]: () => <div>File not found</div>,
+                        }));
+                    });
+            }
+        });
+    }, [openedFiles]);
+
     return (
         <div className='lg:w-[25rem] xl:w-[35rem] 2xl:w-[45rem] 3xl:w-[50rem] border-r border-borderColor h-full'>
             {/* TAB FOR DISPLAYING FILES OPENED */}
-            <div className='border-b border-borderColor text-primary pt-[59.19px] h-[5.95rem] hidden lg:flex items-center'>
-                display
+            <div className='border-b border-borderColor text-primary pt-[59.19px] h-[5.95rem] hidden lg:flex items-center overflow-x-auto'>
+                {openedFiles.map((file) => (
+                    <div
+                        key={file}
+                        className={`text-sm flex items-center px-4 py-2 cursor-pointer border-borderColor border-r ${activeFile === file ? 'text-white' : ''}`}
+                        onClick={() => openFile(file)}
+                    >
+                        <span>{file}</span>
+                        <IoMdClose size={16} className='ml-2' onClick={(e) => { e.stopPropagation(); closeFile(file); }} />
+                    </div>
+                ))}
             </div>
-            {/* CONTENTs BEING DISPLAYED */}
+            {/* CONTENT BEING DISPLAYED */}
             <div className='mx-8 mt-14 lg:mx-0 lg:mt-0 lg:border-r lg:border-borderColor lg:h-[calc(100%-95px)] lg:w-[23rem] xl:w-[33rem] 2xl:w-[43rem] 3xl:w-[48rem]'>
-                display2
+                <div className='pt-8 pl-8 w-[45rem] text-sm'>
+                    {activeFile && components[activeFile] ? React.createElement(components[activeFile]) :
+                        <div>
+                        /** <br />
+                            * No editor open, Click a file to read about me <br />
+                            */<br />
+                        </div>
+                    }
+                </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AboutDisplay
+export default AboutDisplay;
