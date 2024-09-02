b50004942b94e53b5ec965cb8a2f524b050d3b67
vicktor349 <ayomidevictor349@gmail.com>
2024-09-02 11:25:58 +0100

    Personal Data

---
+++ data/personal/Bio.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Bio = () =>
+{
+    return (
+        <div>
+          /**
+            * About
+            * Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,
+            * libero fugiat ullam, omnis minima officia impedit porro eum c-
+            * umque voluptatem aspernatur, tenetur qui maiores. Assumenda qu-
+            * ibusdam consequuntur maxime accusamus nihil.Lorem ipsum dolor
+            * sit amet, consectetur adipisicing elit. Eos, libero fugiat ull-
+            * am, omnis minima officia impedit porro eum cumque voluptatem a-
+            * spernatur, tenetur qui maiores. Assumenda quibusdam consequunt-
+            * ur maxime accusamus nihil.
+            */
+        </div>
+    )
+}
+
+export default Bio
