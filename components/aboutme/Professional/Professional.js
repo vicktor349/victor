0d4810408bd6d42ce5c5bfb2cc5d3772c6084f5e
vicktor349 <ayomidevictor349@gmail.com>
2024-09-02 23:50:11 +0100

    adjust folder padding for smaller screens

--- components/aboutme/Professional/Professional.js
+++ components/aboutme/Professional/Professional.js
@@ -3,12 +3,14 @@ import { FaFolder, FaFolderOpen } from 'react-icons/fa'
 import { MdArrowDropDown, MdKeyboardArrowDown, MdKeyboardArrowRight, MdOutlineArrowRight } from 'react-icons/md'
 import { RiJavascriptFill } from 'react-icons/ri'
 import { PiFileCSharpDuotone } from "react-icons/pi";
+import { useFile } from '@/components/FileContext';
 
 const Professional = () =>
 {
     const [open, setOpen] = useState(true)
     const [frontend, setFrontend] = useState(false)
     const [backend, setBackEnd] = useState(false)
+    const { openFile } = useFile();
     const frontEndToggle = () =>
     {
         setFrontend(!frontend)
@@ -35,7 +37,7 @@ const Professional = () =>
             </div>
             <div>
                 {open &&
-                    <div className='pl-7 mt-2'>
+                    <div className='ssm:pl-14 lg:pl-7 sm:mt-2'>
                         <div className='flex items-center text-primary hover:text-white hover:cursor-pointer w-fit space-x-2 text-sm' onClick={frontEndToggle}>
                             {frontend ? <MdKeyboardArrowDown size={14} /> : <MdKeyboardArrowRight size={14} />}
                             {frontend ? <FaFolderOpen color='#E99287' size={14} /> : <FaFolder size={14} color='#E99287' />}
@@ -44,24 +46,9 @@ const Professional = () =>
                     </div>
                 }
                 {frontend &&
-                    <div className='ml-14 mt-1 flex items-center space-x-1 text-sm text-primary hover:text-white w-fit hover:cursor-pointer'>
+                    <div onClick={() => openFile('Next.js')} className='ml-14 mt-1 flex items-center space-x-1 text-sm text-primary hover:text-white w-fit hover:cursor-pointer'>
                         <RiJavascriptFill color='#f0db4f ' />
-                        <p>React.js</p>
-                    </div>
-                }
-                {open &&
-                    <div className='pl-7 mt-2'>
-                        <div className='flex items-center text-primary hover:text-white hover:cursor-pointer w-fit space-x-2 text-sm' onClick={backEndToggle}>
-                            {backend ? <MdKeyboardArrowDown size={14} /> : <MdKeyboardArrowRight size={14} />}
-                            {backend ? <FaFolderOpen color='#3A49A4' size={14} /> : <FaFolder size={14} color='#3A49A4' />}
-                            <p>backend</p>
-                        </div>
-                    </div>
-                }
-                {backend &&
-                    <div className='ml-14 mt-1 flex items-center space-x-1 text-sm text-primary hover:text-white w-fit hover:cursor-pointer'>
-                        <PiFileCSharpDuotone color='#f0db4f ' />
-                        <p>c#</p>
+                        <p>Next.js</p>
                     </div>
                 }
             </div>
