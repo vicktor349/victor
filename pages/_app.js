687c23f9a018e070605c67c3d73897037b1dd0d7
vicktor349 <ayomidevictor349@gmail.com>
2024-09-02 21:48:57 +0100

    File provider context

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { FileProvider } from "@/components/FileContext";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps })
 {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <FileProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </FileProvider>
   )
 }
