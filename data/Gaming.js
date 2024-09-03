5da1cf93efdea8808daea89d54a64155cc1c2574
vicktor349 <ayomidevictor349@gmail.com>
2024-09-03 11:27:30 +0100

    Data for Hobbies in _about.me

---
+++ data/Gaming.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Gaming = () =>
+{
+    return (
+        <div>
+            <p> /**</p>
+            <p>* About</p>
+            <p>* Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos,</p>
+            <p>* libero fugiat ullam, omnis minima officia impedit porro eum c-</p>
+            <p>* umque voluptatem aspernatur, tenetur qui maiores. Assumenda qu-</p>
+            <p> * ibusdam consequuntur maxime accusamus nihil.Lorem ipsum dolor</p>
+            <p>* sit amet, consectetur adipisicing elit. Eos, libero fugiat ull-</p>
+            <p>* am, omnis minima officia impedit porro eum cumque voluptatem a-</p>
+            <p> * spernatur, tenetur qui maiores. Assumenda quibusdam consequunt-</p>
+            <p>* ur maxime accusamus nihil.</p>
+            <p>*/</p>
+        </div>
+    )
+}
+
+export default Gaming
