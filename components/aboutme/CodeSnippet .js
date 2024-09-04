d6aee6db04c9a6188f52b97b64f1375f944067b4
vicktor349 <ayomidevictor349@gmail.com>
2024-09-04 21:56:57 +0100

    Replaced snippet with spotify playlist

--- components/aboutme/CodeSnippet .js
+++ components/aboutme/CodeSnippet .js
@@ -5,26 +5,13 @@ const CodeSnippet = () =>
 {
     return (
         <div className='border-t border-borderColor text-sm pt-8'>
-            <div className='w-fit grid mx-auto'>
-                <p>// Code Snippet showcase</p>
-                <div className='border-borderColor rounded-xl w-fit border mt-10'>
-                    <GitHubLineSnippet
-                        owner="vicktor349"
-                        repo="nestmart"
-                        filePath="components/CartContext.js"
-                        startLine={23}
-                        endLine={36}
-                    />
-                </div>
-                <div className='border-borderColor rounded-xl w-fit border mt-10'>
-                    <GitHubLineSnippet
-                        owner="vicktor349"
-                        repo="nestmart"
-                        filePath="components/CartContext.js"
-                        startLine={23}
-                        endLine={36}
-                    />
-                </div>
+            <div className='w-full grid mx-auto px-10 pb-10 lg:pb-0'>
+                <p className="font-bold">Hit play and let the rhythm of creativity guide you through my portfolio.</p>
+                <iframe src="https://open.spotify.com/embed/playlist/6V4siH7uJC3R19AFH8R4kr?utm_source=generator&theme=0"
+                    width="100%" height="450"
+                    frameBorder="0" allowFullScreen=""
+                    className="mt-6"
+                    allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
         </div>
     )
