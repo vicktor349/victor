f641523eb42d8975389c9428e71ae0105476f79f
vicktor349 <ayomidevictor349@gmail.com>
2024-09-05 16:53:32 +0100

    Mantine Provider wrapper for accessing mantine globally

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import { FileProvider } from "@/components/FileContext";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
+import { MantineProvider } from "@mantine/core";
 
 export default function App({ Component, pageProps })
 {
   return (
-    <FileProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </FileProvider>
+    <MantineProvider>
+      <FileProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </FileProvider>
+    </MantineProvider>
   )
 }
