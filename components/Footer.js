9e2013624d0753fde9cf73219843099010f16c4b
vicktor349 <ayomidevictor349@gmail.com>
2024-09-06 11:15:03 +0100

    Connected my link to my social handles

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react'
 import { BsGithub, BsTwitterX, BsWhatsapp } from 'react-icons/bs'
 
@@ -10,15 +11,15 @@ const Footer = () =>
                     <p className="ssm:w-32 ssssm:w-44 2xl:w-48 3xl:w-56 border-r border-borderColor h-full flex items-center justify-center">
                         Find me in:
                     </p>
-                    <div className="border-r border-borderColor flex items-center justify-center h-full ssm:w-12 ssssm:w-14">
+                    <Link href="https://x.com/victoh349" target="_blank" rel="noopener noreferrer" className="border-r border-borderColor flex items-center justify-center h-full ssm:w-12 ssssm:w-14">
                         <BsTwitterX />
-                    </div>
-                    <div className="border-r border-borderColor flex items-center justify-center h-full ssm:w-12 ssssm:w-14">
+                    </Link>
+                    <Link href="https://wa.me/08154552075" target="_blank" rel="noopener noreferrer" className="border-r border-borderColor flex items-center justify-center h-full ssm:w-12 ssssm:w-14">
                         <BsWhatsapp />
-                    </div>
+                    </Link>
                 </div>
                 <div className="flex items-center justify-center sssm:w-40 ssssm:w-48 space-x-3 border-borderColor ssssm:border-l">
-                    <p>@vicktor349</p>
+                    <Link href="https://github.com/vicktor349" target="_blank" rel="noopener noreferrer">@vicktor349</Link>
                     <BsGithub />
                 </div>
             </div>
