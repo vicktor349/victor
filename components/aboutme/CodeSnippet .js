5cba32e257d72d09598b945ba5df949a56d846fe
vicktor349 <ayomidevictor349@gmail.com>
2024-09-07 20:12:56 +0100

    Added animation for text

--- components/aboutme/CodeSnippet .js
+++ components/aboutme/CodeSnippet .js
@@ -4,9 +4,9 @@ const CodeSnippet = () =>
 {
     return (
         <div className='border-t border-borderColor text-sm pt-8'>
-            <div className='w-full grid xl:mx-auto px-3 pb-10 lg:pb-0'>
-                <p className="text-lg font-bold">
-                    Hit play and let the <span>rhythm of creativity</span> guide you through my portfolio.
+            <div className='w-full grid mx-auto px-10 pb-10 lg:pb-0'>
+                <p class="text-lg drum-text">
+                    <span>H</span><span>i</span><span>t</span> <span>p</span><span>l</span><span>a</span><span>y</span> <span>a</span><span>n</span><span>d</span> <span>l</span><span>e</span><span>t</span> <span>t</span><span>h</span><span>e</span> <span>r</span><span>y</span><span>t</span><span>h</span><span>m</span> <span>o</span><span>f</span> <span>c</span><span>r</span><span>e</span><span>a</span><span>t</span><span>i</span><span>v</span><span>i</span><span>t</span><span>y</span> <span>f</span><span>l</span><span>o</span><span>w</span>
                 </p>
                 <iframe src="https://open.spotify.com/embed/playlist/6V4siH7uJC3R19AFH8R4kr?utm_source=generator&theme=0"
                     className="mt-6 h-[30rem] w-full">
