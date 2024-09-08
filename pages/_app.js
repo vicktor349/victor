e9d96eb3f5f6d23f24f9110776dd9214a7f33a90
vicktor349 <ayomidevictor349@gmail.com>
2024-09-08 14:19:51 +0100

    Added mantine notification as global import

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,13 @@ import { FileProvider } from "@/components/FileContext";
 import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 import { MantineProvider } from "@mantine/core";
+import { Notifications } from '@mantine/notifications';
 
 export default function App({ Component, pageProps })
 {
   return (
     <MantineProvider>
+      <Notifications />
       <FileProvider>
         <Layout>
           <Component {...pageProps} />
