5d0cad42dcc8e84876609c746dbd5239d59c2f61
vicktor349 <ayomidevictor349@gmail.com>
2024-09-08 14:20:24 +0100

    changed crossorigin to crossOrigin

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document()
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300..700&display=swap" rel="stylesheet" />
       </Head>
       <body>
